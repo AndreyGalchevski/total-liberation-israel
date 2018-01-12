ccd70539510632a434640a3b1b582086379f58d5
Andrey Galchevski <andreygalchevski@gmail.com>
2018-01-12 16:10:10 +0200

    connected to prod db on digital ocean

--- config/db.js
+++ config/db.js
@@ -1,4 +1,8 @@
 module.exports = {
-    url: 'mongodb://admin:1234@ds259865.mlab.com:59865/alf-israel',
+    url: 'mongodb://admin:1234@165.227.232.189:27017/alf-israel',
     secret: 'yoursecret'
 }
+// module.exports = {
+//     url: 'mongodb://admin:1234@ds259865.mlab.com:59865/alf-israel',
+//     secret: 'yoursecret'
+// }
