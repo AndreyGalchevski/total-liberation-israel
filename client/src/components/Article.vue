50e910cc6f236cdff9f0e35fd672bda4c1f87403
Andrey Galchevski <andreygalchevski@gmail.com>
2018-01-15 07:48:40 +0200

    fixed the sharing bug

--- client/src/components/Article.vue
+++ client/src/components/Article.vue
@@ -15,28 +15,28 @@
       h6(class="date text-primary") פורסם ב - {{ getDate(thisArticle.date) }} מאת {{ thisArticle.author }}
       img(class="rounded main-image img-fluid" :src = "thisArticle.image" alt="Card image")
       p(class="share-buttons")
-        a(:href="'mailto:?subject=' + thisArticle.title + '&body=https://alf-israel.com/article' + thisArticle._id")
+        a(:href="'mailto:?subject=' + thisArticle.title + '&body=https://www.alf-israel.com/article/' + thisArticle._id")
           i(class="fa fa-envelope-o top-icon" aria-hidden="true")
         a(class="fb-xfbml-parse-ignore" target="_blank" 
-        :href="'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Falf-israel.com%2Farticle%2F&amp;src=sdkpreparse' + thisArticle._id + '&amp;src=sdkpreparse'")
+        :href="'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.alf-israel.com%2Farticle%2F&amp;src=sdkpreparse' + thisArticle._id + '&amp;src=sdkpreparse'")
           i(class="fa fa-facebook-square top-facebook-icon top-icon" aria-hidden="true")
-        a(v-if="this.screenWidth < 700" :href="'whatsapp://send?text=https://alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
+        a(v-if="this.screenWidth < 700" :href="'whatsapp://send?text=https://www.alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
           i(class="fa fa-whatsapp top-icon" aria-hidden="true")
-        a(v-else :href="'https://web.whatsapp.com/send?text=https://alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
+        a(v-else :href="'https://web.whatsapp.com/send?text=https://www.alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
           i(class="fa fa-whatsapp top-icon" aria-hidden="true")
       p(class="my-content") {{ thisArticle.content }}
       div(class="dropdown-share btn-group")
         button(class="btn btn-outline-primary waves-effect" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false")
           i(class="fa fa-share-alt" aria-hidden="true")
         div(class="dropdown-menu")
-          a(class="dropdown-item" :href="'mailto:?subject=' + thisArticle.title + '&body=https://alf-israel.com/article' + thisArticle._id")
+          a(class="dropdown-item" :href="'mailto:?subject=' + thisArticle.title + '&body=https://www.alf-israel.com/article/' + thisArticle._id")
             i(class="fa fa-envelope-o bottom-icon" aria-hidden="true")
           a(class="dropdown-item fb-xfbml-parse-ignore" target="_blank" 
-          :href="'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Falf-israel.com%2Farticle%2F&amp;src=sdkpreparse' + thisArticle._id + '&amp;src=sdkpreparse'")
+          :href="'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.alf-israel.com%2Farticle%2F&amp;src=sdkpreparse' + thisArticle._id + '&amp;src=sdkpreparse'")
              i(class="fa fa-facebook-square bottom-icon" aria-hidden="true")
-          a(v-if="this.screenWidth < 700" class="dropdown-item" :href="'whatsapp://send?text=https://alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
+          a(v-if="this.screenWidth < 700" class="dropdown-item" :href="'whatsapp://send?text=https://www.alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
             i(class="fa fa-whatsapp bottom-icon" aria-hidden="true")
-          a(v-else class="dropdown-item" :href="'https://web.whatsapp.com/send?text=https://alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
+          a(v-else class="dropdown-item" :href="'https://web.whatsapp.com/send?text=https://www.alf-israel.com/article/' + thisArticle._id" data-action="share/whatsapp/share" target="_blank")
             i(class="fa fa-whatsapp bottom-icon" aria-hidden="true")
 </template>
 
