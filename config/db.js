88ba409dafc89155bbc531066f7192d5b451e1cf
Andrey Galchevski <andreygalchevski@gmail.com>
2018-01-30 22:16:24 +0200

    changed mongodb hosting service

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: 'mongodb://admin:1234@165.227.232.189:27017/alf-israel',
+    url: 'mongodb://heroku_4jxv0rrc:ruaildt1fbrf2s3minaguujvq8@ds221028-a0.mlab.com:21028,ds221028-a1.mlab.com:21028/heroku_4jxv0rrc?replicaSet=rs-ds221028',
     secret: 'yoursecret'
 }
 // module.exports = {
