d0f9764788899fe2f5ea30aad0e9ac8fc85e93b7
Andrey Galchevski <andreygalchevski@gmail.com>
2018-02-15 23:18:19 +0200

    make it much much much better. ssr style better

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/dist/client-bundle.0e5c151e2894518253cb.js","318639ec06d9715c0cc25f266226b140"],["/dist/client-bundle.e4d644ae5d1bd79a4e3a.js","bcf1829a231ba96921652f462573e9df"]];
+var precacheConfig = [["/dist/client-bundle.0e5c151e2894518253cb.js","318639ec06d9715c0cc25f266226b140"],["/dist/client-bundle.954079ec785d7ac73992.js","971d14585d2a269c25a4e7e6f20bd1b2"]];
 var cacheName = 'sw-precache-v3-vue-hn-' + (self.registration ? self.registration.scope : '');
 
 
