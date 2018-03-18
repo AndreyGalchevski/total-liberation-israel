4043f674697eebc2adb98b8d8a46c22aeff6e329
andreygalchevski <andreygalchevski@gmail.com>
2018-03-18 22:08:41 +0200

    fixed swiper import jihadish issue

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -44,11 +44,6 @@ module.exports = {
         options: vueConfig
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
