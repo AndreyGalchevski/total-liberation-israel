48de9c67e6b08c089f4309b72af94667da2dfbed
Andrey Galchevski <agalchevski@heed-global.com>
2019-01-08 18:09:21 +0200

    migrate about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,19 @@
 <template lang="pug">
 div.about
-	div.container
-		div.row
-			div.col-sm-2
-			div.col-sm-8
-				h1 מי אנחנו?
-				p.
-					תנועת שחרור מוחלט הוקמה במרץ 2018 על ידי חבורת אקטיביסטים ממובילי מהפכת שחרור בעלי החיים בישראל.
-					התנועה בנויה מתאי פעילות אזוריים בכל רחבי הארץ ואנו פועלים במגוון אירועי שטח כמו מחאות והפגנות רועשות ושקטות, מייצגי רחוב, פעילות הסברה מול בני נוער בתיכונים, פעילים מתעדים במשקים בהם כלואים בעלי החיים, חלוקת מזון טבעוני בבתי חולים ועוד אינספור פרויקטים, שכל מטרתם השמעת קולם של בעלי החיים הסובלים והנטבחים בכל מקום ובכל זמן.
-				p מוזמנים להצטרף אלינו. הירשמו לקבוצת הפעילות שלנו ב
-					a(href="https://www.drove.com/vegans" target="_blank") דרוב.
-				p מלאו את פרטי ההתקשרות שלכם
-					router-link(to="/contact")  כאן
-					span   ונצרף אתכם לתאי הפעילות האזוריים.
-				img.img-fluid(
-					src="https://res.cloudinary.com/dqvimfd8b/image/upload/v1515705896/alf-israel/about/about.jpg"
-				)
-			div.col-sm-2
+	div.container.flow-text
+		h3 מי אנחנו?
+		p.
+			תנועת שחרור מוחלט הוקמה במרץ 2018 על ידי חבורת אקטיביסטים ממובילי מהפכת שחרור בעלי החיים בישראל.
+			התנועה בנויה מתאי פעילות אזוריים בכל רחבי הארץ ואנו פועלים במגוון אירועי שטח כמו מחאות והפגנות רועשות ושקטות, מייצגי רחוב, פעילות הסברה מול בני נוער בתיכונים, פעילים מתעדים במשקים בהם כלואים בעלי החיים, חלוקת מזון טבעוני בבתי חולים ועוד אינספור פרויקטים, שכל מטרתם השמעת קולם של בעלי החיים הסובלים והנטבחים בכל מקום ובכל זמן.
+		p מוזמנים להצטרף אלינו. הירשמו לקבוצת הפעילות שלנו ב
+			a(href="https://www.drove.com/vegans" target="_blank") דרוב.
+		p מלאו את פרטי ההתקשרות שלכם
+			router-link(to="/contact")  כאן
+			span   ונצרף אתכם לתאי הפעילות האזוריים.
+		div
+			img.responsive-img(
+				src="https://res.cloudinary.com/dqvimfd8b/image/upload/v1515705896/alf-israel/about/about.jpg"
+			)
 </template>
 
 <script>
@@ -29,17 +26,17 @@ export default {
 </script>
 
 <style scoped>
-.container {
-	font-size: 17px;
-	padding-top: 15px;
-	padding-bottom: 30px;
+img {
+	/* font-size: 17px; */
+	/* padding-top: 15px; */
+	padding-bottom: 80px;
 }
 p {
 	text-align: right;
 }
-h1 {
+/* h1 {
 	padding-bottom: 20px;
-}
+} */
 </style>
 
 
