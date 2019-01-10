c0532521333186068fc9b180460da382f6398cca
Andrey Galchevski <agalchevski@heed-global.com>
2019-01-10 17:51:34 +0200

    latest build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/dist/client-bundle.665e97a2dcbf794f18a6.js","4b862f01ffc90531ede77a8f33e2de7b"],["/dist/client-bundle.dc9f2a3f58ed68162b85.js","0e2fc9ff10b84884dd1c8bbf978ab987"]];
+var precacheConfig = [["/dist/client-bundle.0a9655e2a309da4c9494.js","0d9cabb030e9ca8393dce6925681da41"],["/dist/client-bundle.665e97a2dcbf794f18a6.js","4b862f01ffc90531ede77a8f33e2de7b"]];
 var cacheName = 'sw-precache-v3-vue-hn-' + (self.registration ? self.registration.scope : '');
 
 
