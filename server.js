31b4bddb6ad7d3ac4ae8ff5be1e45a7d1132f61b
Andrey Galchevski <agalchevski@heed-global.com>
2019-01-21 08:36:32 +0200

    change server port to 4000

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('*', (req, res) => {
 		.pipe(res)
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
 	console.log(`server started on ${port}`)
 })
